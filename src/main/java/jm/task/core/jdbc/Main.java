f159c56d3746aab8462cf062750676e254aaecb6
life with java <pozigun.92@mail.ru>
2022-07-01 20:21:38 +0300

    Пишет что версия актуальна, после того как добавлю комент нормально обновляется.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.dropUsersTable();
+        userService.createUsersTable();
+        userService.saveUser(" Чарли ", " Вильямс ", (byte) 25);
+        userService.saveUser(" Джек ", " Петерс ", (byte) 26);
+        userService.saveUser(" Гарри ", " Гибсон ", (byte) 27);
+        userService.saveUser(" Джейкоб ", " Мартин ", (byte) 28);
+        userService.removeUserById(3);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,112 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    SessionFactory sessionFactory = Util.getSessionFactory();
+    Transaction transaction;
+
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
 
+
+        try {
+            Session session = sessionFactory.openSession();// связь с базой данных
+            transaction = session.beginTransaction();//Начинает транзакцию базы данных.
+            Query query = session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +//собственный запрос оператора SQL, после запроса возвращается объект Object
+                    "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
+                    "age TINYINT NOT NULL)");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
+
 
     @Override
     public void dropUsersTable() {
+
+        try {
+            Session session = sessionFactory.openSession();
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createSQLQuery("DROP TABLE IF EXISTS users");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            Session session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            System.out.println("User с именем – " + name + " добавлен в базу данных ");
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try {
+            Session session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try {
+            Session session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            users = (List<User>) session.createQuery("From User").list();
+            System.out.println(users);
+            transaction.commit();
+
+        } catch (HibernateException e) {
+            transaction.rollback();
+        }
+
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try {
+            Session session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            Query query = session.createSQLQuery("TRUNCATE TABLE users");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 }
